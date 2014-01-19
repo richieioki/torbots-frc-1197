70ad8fdc8a96306a4ec42e71e728d96a0580e26a
richie.ioki@gmail.com <richie.ioki@gmail.com@7d0f5252-9077-d4ed-bbc1-f64cc19679cd>
2014-01-19 18:53:39 +0000

    Inserted Drive Code and implemented shifting.  Tested on 2013 practice bot. -Richie

--- trunk/AerialAssist/2014AerialAssist/Autonomous.h
+++ trunk/AerialAssist/2014AerialAssist/Autonomous.h
@@ -12,14 +12,15 @@ class Autonomous {
   
   public:
     
-    Autonomous(TorDriveTrain &drive, TorShooter &shooter);
+    //Autonomous(TorDriveTrain &drive, TorShooter &shooter);
     ~Autonomous();
-    void runAutonomous();  //is called to run the various auto modes.
+    void runAutonomous();  //is called to run the various auto modes.  Function must access
   
   private:
     
-    void AutoMode1();
-    void AutoMode2();
-    void AutoMode3();
+    void AutoMode1(); //just drive forward
+    void AutoMode2(); //target and fire, left.  Then drive forward
+    void AutoMode3(); //target and fire right.  Then drive forward
+    void AutoMode4(); //target and fire center.  Then drive forward
     
 };
