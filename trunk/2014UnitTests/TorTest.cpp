ccfa802c4cf9f256c2514212bab91b72764ea998
richie.ioki@gmail.com <richie.ioki@gmail.com@7d0f5252-9077-d4ed-bbc1-f64cc19679cd>
2014-02-06 03:03:04 +0000

    Adding 2014 Unit Tests - Brandon

---
+++ trunk/2014UnitTests/TorTest.cpp
@@ -0,0 +1,20 @@
+#include "WPILib.h"
+#include "TorDrive.h"
+#include "TorTest.h"
+
+  
+TorTest::TorTest(Joystick& theStick, DriverStationLCD *dsIn, TorDrive *DriveIn)
+: m_stick(theStick), m_ds(dsIn), m_myTorDrive(DriveIn)
+{
+ 
+}
+void TorTest::testDrive()
+{
+  m_myTorDrive->SetDrive(0.5,0.5);
+}
+void TorTest::testSensors()
+{
+  
+}
+
+
