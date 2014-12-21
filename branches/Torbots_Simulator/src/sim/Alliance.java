b256e0b21a36cc090ecf2b4aa889e653931a1fa8
richie.ioki@gmail.com <richie.ioki@gmail.com@7d0f5252-9077-d4ed-bbc1-f64cc19679cd>
2014-12-21 23:54:36 +0000

    Updated Robot Simulator

---
+++ branches/Torbots_Simulator/src/sim/Alliance.java
@@ -0,0 +1,11 @@
+package sim;
+
+import objects.Robot;
+
+public class Alliance {
+
+    Alliance(String r, Robot red1, Robot red2, Robot red3) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
---
+++ branches/Torbots_Simulator/src/sim/Data.java
@@ -0,0 +1,11 @@
+package sim;
+
+/**
+ * Container class for data collected in the match, mostly points for now
+ * @author Richie Ioki
+ */
+class Data {
+    private int pointsScored;
+    private int autoPoints;
+    
+}
